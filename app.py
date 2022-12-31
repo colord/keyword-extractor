44db486c5911fee4b091c36e28b013b9c1bcdf8c
colord <10601293+colord@users.noreply.github.com>
2022-12-31 15:04:19 -0800

    Create job descriptions. Read through txt file

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import spacy
+import textacy
+import os
+import pprint
+from textacy.extract import keyterms as kt
 
-spacy.prefer_gpu()
-# nlp = spacy.load("en_core_web_sm")
+pp = pprint.PrettyPrinter(indent=4)
+
+def load_text(filename):
+    text = ""
+    path = os.path.join("./job-descriptions", filename)
+    with open(path, 'r') as f:
+        text = f.read()
+    return text
+
+text = load_text('job1.txt')
+
+en = textacy.load_spacy_lang("en_core_web_lg", disable=("parser",))
+doc = textacy.make_spacy_doc(text, lang=en)
+
+pp.pprint(kt.textrank(doc, normalize="lemma", topn=10))
